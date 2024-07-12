5575e9c7f5310cefb1b97de52f74da34e6f04720
Finn <git@finnley.dev>
2024-07-12 15:00:31 +0200

    make check insensitive

--- checkduplicates.sh
+++ checkduplicates.sh
@@ -4,7 +4,7 @@
 FILE_PATH="usernames"
 
 # Überprüfe, ob doppelte Zeilen vorhanden sind
-DUPLICATE_LINES=$(sort "$FILE_PATH" | uniq -d)
+DUPLICATE_LINES=$(sort -f "$FILE_PATH" | uniq -id)
 
 if [ "$DUPLICATE_LINES" ]; then
     echo "##### Duplicates discovered! #####"
